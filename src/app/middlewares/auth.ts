bc83a7eaad3efe2c8ae9ecaca9e7ded108717a74
Noyon Hossain <noyonhossain560@gmail.com>
2025-04-13 10:21:15 +0600

    Creating Reusable Authentication Middleware and Handling API Errors

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -2,18 +2,20 @@ import { NextFunction, Request, Response } from "express";
 import { JwtHelpers } from "../../helpers/jwtHelpers";
 import config from "../../config";
 import { Secret } from "jsonwebtoken";
+import ApiError from "../errors/ApiError";
+import httpStatus from "http-status";
 
 const auth = (...roles: string[]) => {
     return async (req: Request, res: Response, next: NextFunction) => {
         try {
             const token = req.headers.authorization;
             if (!token) {
-                throw new Error("Access denied. Invalid or missing authorization token.");
+                throw new ApiError(httpStatus.UNAUTHORIZED, "Access denied. Invalid or missing authorization token.");
             }
 
             const verifiedUser = JwtHelpers.verifyToken(token, config.jwt.access_token_secrte as Secret);
             if (roles.length && !roles.includes(verifiedUser?.role)) {
-                throw new Error(`Access denied. Your role '${verifiedUser.role}' does not have permission to perform this action.`);
+                throw new ApiError(httpStatus.FORBIDDEN, `Access denied. Your role '${verifiedUser.role}' does not have permission to perform this action.`);
             }
 
             next();
