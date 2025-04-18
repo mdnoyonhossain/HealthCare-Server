beca5c34d33cfbbfb5206940090641c0fa20f187
Noyon Hossain <noyonhossain560@gmail.com>
2025-04-18 08:46:44 +0600

    Adding Type and Zod Schema to Specialties

--- src/app/modules/Specialties/specialties.routes.ts
+++ src/app/modules/Specialties/specialties.routes.ts
@@ -3,6 +3,7 @@ import { SpecialtiesController } from "./specialties.controller";
 import auth from "../../middlewares/auth";
 import { UserRole } from "@prisma/client";
 import { fileUploader } from "../../../helpers/fileUploader";
+import { SpecialtiesValidation } from "./specialties.validation";
 
 const router = express.Router();
 
@@ -11,7 +12,7 @@ router.post(
     // auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
     fileUploader.upload.single('file'),
     (req: Request, res: Response, next: NextFunction) => {
-        req.body = JSON.parse(req.body.data);
+        req.body = SpecialtiesValidation.createSpecialtiesValidationSchema.parse(JSON.parse(req.body.data));
         return SpecialtiesController.specialtiesInsertIntoDB(req, res, next);
     }
 );
