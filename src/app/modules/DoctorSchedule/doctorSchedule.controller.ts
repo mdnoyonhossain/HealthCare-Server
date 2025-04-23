ba90a345703c63bc815e130a04db609168892f81
Noyon Hossain <noyonhossain560@gmail.com>
2025-04-23 08:27:36 +0600

    Retrieving Doctor Schedule with Filtering

--- src/app/modules/DoctorSchedule/doctorSchedule.controller.ts
+++ src/app/modules/DoctorSchedule/doctorSchedule.controller.ts
@@ -4,6 +4,7 @@ import sendResponse from "../../../shared/sendResponse";
 import { DoctorScheduleService } from "./doctorSchedule.service";
 import { Request } from "express";
 import { TAuthUser } from "../../interfaces/common";
+import pick from "../../../shared/pick";
 
 const createDoctorSchedule = catchAsync(async (req: Request & { user?: TAuthUser }, res) => {
     const user = req.user;
@@ -18,6 +19,21 @@ const createDoctorSchedule = catchAsync(async (req: Request & { user?: TAuthUser
     });
 });
 
+const getMyDoctorScheduleFromDB = catchAsync(async (req: Request & { user?: TAuthUser }, res) => {
+    const filters = pick(req.query, ['startDate', 'endDate', 'isBooked']);
+    const options = pick(req.query, ['page', 'limit', 'sortBy', 'sortOrder']);
+    const user = req.user;
+    const result = await DoctorScheduleService.getMyDoctorScheduleFromDB(filters, options, user as TAuthUser);
+
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "My Schedule data retrieved successfully.",
+        data: result
+    });
+});
+
 export const DoctorScheduleController = {
-    createDoctorSchedule
+    createDoctorSchedule,
+    getMyDoctorScheduleFromDB
 }
