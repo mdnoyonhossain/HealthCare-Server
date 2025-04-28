b15e31f244d9fd6fd3a11758dd4c830d7d9c7e84
Noyon Hossain <noyonhossain560@gmail.com>
2025-04-28 12:09:21 +0600

    Prescription Validation added

--- src/app/modules/Prescription/prescription.routes.ts
+++ src/app/modules/Prescription/prescription.routes.ts
@@ -2,6 +2,8 @@ import express from "express";
 import { PrescriptionController } from "./prescription.controller";
 import auth from "../../middlewares/auth";
 import { UserRole } from "@prisma/client";
+import validateRequest from "../../middlewares/validateRequest";
+import { PrescriptionValidation } from "./prescription.validation";
 
 const router = express.Router();
 
@@ -14,6 +16,7 @@ router.get(
 router.post(
     '/',
     auth(UserRole.DOCTOR),
+    validateRequest(PrescriptionValidation.createPrescriptionValidationSchema),
     PrescriptionController.createPrescription
 );
 
