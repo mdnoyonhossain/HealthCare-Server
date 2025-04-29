c3534b4d07b44f6910111211ac99c73f579c3f92
Noyon Hossain <noyonhossain560@gmail.com>
2025-04-29 10:36:29 +0600

    Seeding Super Admin Data into the Database

---
+++ prisma/seed.ts
@@ -0,0 +1,45 @@
+import { UserRole } from "@prisma/client";
+import prisma from "../src/shared/prisma";
+import bcrypt from "bcrypt";
+import config from "../src/config";
+
+const seedSuperAdmin = async () => {
+    try {
+        const isExistSuperAdmin = await prisma.user.findFirst({
+            where: {
+                role: UserRole.SUPER_ADMIN
+            }
+        });
+
+        if (isExistSuperAdmin) {
+            console.log("Super Admin already exists.");
+            return;
+        }
+
+        const hashedPassword = await bcrypt.hash("superadmin", Number(config.bcrypt_salt_round));
+
+        const superAdminData = await prisma.user.create({
+            data: {
+                email: "super@admin.com",
+                password: hashedPassword,
+                role: UserRole.SUPER_ADMIN,
+                admin: {
+                    create: {
+                        name: "Super Admin",
+                        contactNumber: "01723455790"
+                    }
+                }
+            }
+        });
+
+        console.log("Super Admin seeded successfully!", superAdminData);
+    }
+    catch (err: any) {
+        throw new Error(`Seeding Super Admin failed. ${err.message}`);
+    }
+    finally {
+        await prisma.$disconnect();
+    }
+}
+
+seedSuperAdmin();
